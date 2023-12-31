16ef472340fc21c841d25a7120a64e82615b0dcc
vinaykumar7580 <vinayhatwar16@gmail.com>
2023-12-31 15:15:46 +0530

    added counter app created using useRef hook

--- problems-practise/react/my-app/src/App.js
+++ problems-practise/react/my-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter1 from './Components/Counter1';
+import Counter2 from './Components/Counter2';
 
 function App() {
   return (
     <div className="App">
       <Counter1/>
+      <hr/>
+      <Counter2/>
     </div>
   );
 }
---
+++ problems-practise/react/my-app/src/Components/Counter2.jsx
@@ -0,0 +1,45 @@
+import { useRef, useState } from "react"
+
+
+function Counter2(){
+    const [count, setCount]=useState(0)
+
+    const ref=useRef(null);
+
+    const handleStart=()=>{
+
+        if(ref.current!=null){
+            return
+        }
+
+        ref.current=setInterval(()=>{
+            setCount((prev)=>prev+1)
+
+        }, 1000)
+
+    }
+
+    const handleStop=()=>{
+        clearInterval(ref.current)
+        ref.current=null
+
+    }
+
+    const handleReset=()=>{
+        handleStop();
+        setCount(0)
+
+    }
+
+    return(
+        <div>
+            <h1>Counter with hook</h1>
+            <h3>Count: {count}</h3>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button>
+            <button onClick={handleReset}>Reset</button>
+
+        </div>
+    )
+}
+export default Counter2
